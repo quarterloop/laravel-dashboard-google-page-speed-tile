9ed35ca93dee76b572f541beb8baa9e751783811
Gero Wallisch <gero.wallisch@me.com>
2022-05-03 22:47:39 +0200

    changed get request

--- src/FetchDataFromApiCommand.php
+++ src/FetchDataFromApiCommand.php
@@ -15,9 +15,7 @@ class FetchDataFromApiCommand extends Command
         $this->info('Fetching Page Speed...');
 
 
-        $data = GooglePageSpeedAPI::getPageSpeed(
-            config('dashboard.tiles.google_page_speed.url'),
-            config('dashboard.tiles.google_page_speed.key')
+        $data = Http::get('https://pagespeedonline.googleapis.com/pagespeedonline/v5/runPagespeed?url=https%3A%2F%2Fskouz.de&category=CATEGORY_UNSPECIFIED&strategy=STRATEGY_UNSPECIFIED&key=AIzaSyDHA5nTjanJxbpEwgNtiTT2paVy0H_jtKc')->json();
         );
 
 
