8e6268e5b911b5e7811fa853446ca378b37f893f
Gero Wallisch <gero.wallisch@me.com>
2022-05-16 17:06:30 +0200

    changed api-call to all data

--- src/Services/GooglePageSpeedAPI.php
+++ src/Services/GooglePageSpeedAPI.php
@@ -10,17 +10,13 @@ class GooglePageSpeedAPI
   {
       $apiCallDesktop = "https://pagespeedonline.googleapis.com/pagespeedonline/v5/runPagespeed?url=https%3A%2F%2F{$url}&category=CATEGORY_UNSPECIFIED&strategy=DESKTOP&key={$key}";
 
-      $responseDesktop = Http::get($apiCallDesktop)->json();
-
-      return $responseDesktop['lighthouseResult']['categories']['performance'];
+      return Http::get($apiCallDesktop)->json();
   }
 
   public static function getPageSpeedMobile(string $url, string $key): array
   {
       $apiCallMobile = "https://pagespeedonline.googleapis.com/pagespeedonline/v5/runPagespeed?url=https%3A%2F%2F{$url}&category=CATEGORY_UNSPECIFIED&strategy=DESKTOP&key={$key}";
 
-      $responseMobile = Http::get($apiCallMobile)->json();
-
-      return $responseMobile['lighthouseResult']['categories']['performance'];
+      return Http::get($apiCallMobile)->json();
   }
 }
