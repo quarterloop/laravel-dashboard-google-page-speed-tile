22f1aad3607010f52fc9ac928e15f11349ae679e
Gero Wallisch <gero.wallisch@me.com>
2022-05-16 15:17:30 +0200

    split classes

--- src/Commands/FetchGooglePageSpeedMobileCommand.php
+++ src/Commands/FetchGooglePageSpeedMobileCommand.php
@@ -4,30 +4,7 @@ namespace Quaterloop\GooglePageSpeedTile\Commands;
 
 use Illuminate\Console\Command;
 use Quaterloop\GooglePageSpeedTile\Services\GooglePageSpeedAPI;
-use Quaterloop\GooglePageSpeedTile\GooglePageSpeedDesktopStore;
 use Quaterloop\GooglePageSpeedTile\GooglePageSpeedMobileStore;
-
-class FetchGooglePageSpeedDesktopCommand extends Command
-{
-    protected $signature = 'dashboard:fetch-google-page-speed-desktop-data';
-
-    protected $description = 'Fetch Google Page Speed Desktop data';
-
-    public function handle(GooglePageSpeedAPI $google_page_speed_api)
-    {
-
-      $this->info('Fetching data...');
-
-        $pageSpeedDesktop = $google_page_speed_api::getPageSpeedDesktop(
-            config('dashboard.tiles.google_page_speed.url'),
-            config('dashboard.tiles.google_page_speed.key'),
-        );
-
-        GooglePageSpeedDesktopStore::make()->setData($pageSpeedDesktop);
-
-        $this->info('All done!');
-    }
-}
 
 class FetchGooglePageSpeedMobileCommand extends Command
 {
--- src/Services/GooglePageSpeedDesktopAPI.php
+++ src/Services/GooglePageSpeedDesktopAPI.php
@@ -15,15 +15,3 @@ class GooglePageSpeedDesktopAPI
         return $response['lighthouseResult']['categories']['performance'];
     }
 }
-
-class GooglePageSpeedMobileAPI
-{
-    public static function getPageSpeedMobile(string $url, string $key): array
-    {
-        $apiCall = "https://pagespeedonline.googleapis.com/pagespeedonline/v5/runPagespeed?url=https%3A%2F%2F{$url}&category=CATEGORY_UNSPECIFIED&strategy=MOBILE&key={$key}";
-
-        $response = Http::get($apiCall)->json();
-
-        return $response['lighthouseResult']['categories']['performance'];
-    }
-}
